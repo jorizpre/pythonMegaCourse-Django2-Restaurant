2cf6982bf73cd892a2dc34a9d5f11838a6ab3329
jorizpre <jorizpre@gmail.com>
2023-07-14 15:18:32 +0200

    Creation of context: sending data from Python to UI

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -8,6 +8,11 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")  # querying data from DB table
     template_name = "index.html"  # instead of the "return" instruction of the function-based views
 
+    def get_context_data(self):
+        context = {"meals":"Pizza", # the key is the variable that can be accessed through the html template and the value is what is displayed in the UI
+                   "ingredients":["tomato", "mozzarela"]}
+        return context
+
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
