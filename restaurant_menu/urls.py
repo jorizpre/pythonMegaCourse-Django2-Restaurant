d850cdad2822a2df9eb9e80aefef8f51d47f31c5
jorizpre <jorizpre@gmail.com>
2023-07-15 15:24:02 +0200

    Display content from DB with for loops and if statements, created dynamic links

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -4,5 +4,6 @@ from . import views # . is current directory
 # connecting URLs with Views
 urlpatterns = [
     path("", views.MenuList.as_view(), name="home"), # with function-based views we do not need the as_view method
-    
+    path("item/<int:pk>/", views.MenuItemDetail.as_view(), name="menu_item")
+    # <int:pk> makes the link dynamic. A "primary key" from the Database is expected
 ]
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 # Views can be "function-based" or "class-based" (like this one)
 # In these classes, the variable names need to have these specific names
@@ -8,9 +8,11 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")  # querying data from DB table
     template_name = "index.html"  # instead of the "return" instruction of the function-based views
 
-    def get_context_data(self):
-        context = {"meals":"Pizza", # the key is the variable that can be accessed through the html template and the value is what is displayed in the UI
-                   "ingredients":["tomato", "mozzarela"]}
+    def get_context_data(self, **kwargs):
+        # Creation of "context" dictionary that gets the DB items list:
+        context = super().get_context_data(**kwargs)
+        # Now we start writting other entries on the dictionary:
+        context["meals"] = MEAL_TYPE # first, we want to display the meal categories
         return context
 
 class MenuItemDetail(generic.DetailView):
